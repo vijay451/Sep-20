2148fcfb520ff72aefddbce3511824ebda48cba2
vijayB <88872426+vijay451@users.noreply.github.com>
2021-09-20 10:53:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const container= document.querySelector(".container");
+const seats = document.querySelectorAll(".row seat:not(.occupied)");
+
+container.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("seat")&&!e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected')
+    }
+})
